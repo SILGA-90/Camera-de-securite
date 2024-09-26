5134eb7fc22fab9d3da4bbc2f47c31b7adf1d1d4
SILGA <kingsoulpower@gmail.com>
2024-09-26 19:24:05 +0000

    Import des librairies

---
+++ camera-securite.py
@@ -0,0 +1,9 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+import sys
+import cv2
+import winsound
+
+ui,_=loadUiType('camera-securite.py')
