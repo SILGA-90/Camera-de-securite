f4a1067ef06c1b5892038e23533f155d6c140034
SILGA <kingsoulpower@gmail.com>
2024-09-26 19:41:14 +0000

    Configurer et lancer une application PyQt avec une interface utilisateur définie dans un fichier séparé

--- camera-securite.py
+++ camera-securite.py
@@ -6,4 +6,27 @@ import sys
 import cv2
 import winsound
 
+#Cette ligne charge le fichier de l’interface utilisateur (UI) nommé camera-securite.py. La fonction loadUiType est utilisée pour charger les fichiers .ui créés avec Qt Designer.
 ui,_=loadUiType('camera-securite.py')
+
+"""
+Définition de la classe principale de l’application : MainApp hérite de QMainWindow et de l’interface utilisateur chargée. 
+Le constructeur initialise la fenêtre principale et configure l’interface utilisateur.
+"""
+class MainApp(QMainWindow,ui):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        
+"""
+Fonction principale : La fonction main crée une instance de l’application Qt (QApplication), crée une instance de MainApp, 
+affiche la fenêtre principale et lance la boucle d’événements de l’application avec app.exec_()
+"""   
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp
+    window.show()
+    app.exec_()
+#Exécution du script: Cette condition vérifie si le script est exécuté directement (et non importé comme module), et appelle la fonction main pour démarrer l’application
+if __name__ == '__main__':
+    main()
