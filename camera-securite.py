c54f42a327e32a85d7bb4a16e54a59f08112d7ec
SILGA <kingsoulpower@gmail.com>
2024-09-27 12:26:10 +0000

    "Added motion detection and alert functionality to camera surveillance script"

--- camera-securite.py
+++ camera-securite.py
@@ -34,8 +34,17 @@ class MainApp(QMainWindow,ui):
             diff = cv2.absdiff(im1,im2)
             gray = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
             blur = cv2.GaussianBlur(gray,(5,5),0)
-            
-            cv2.imshow("CAMERA DE SURVEILLANCE",blur)
+            _,thresh = cv2.threshold(blur, 20,255, cv2.THRESH_BINARY)
+            dilated = cv2.dilate(thresh,None,iterations=3)
+            contours,_= cv2.findContours(dilated,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            for c in contours:
+                if cv2.contourArea(c) < 1000:
+                    continue
+                x,y,w,h = cv2.boundingRect(c)
+                cv2.rectangle(im1(x,y),(x+w,y+h),(0,255,2))
+                winsound.Beep(self.volume,200)
+            cv2.imshow("CAMERA DE SURVEILLANCE",im1)
+    
             key = cv2.waitKey(10)
             if key == 27:
                 break
